751b05da194e4378cdc5dfa783ee1b69c120b33f
amritpalkaur080 <85336481+amritpalkaur080@users.noreply.github.com>
2021-06-04 13:00:07 +0530

    Add files via upload

---
+++ MyGame.java
@@ -0,0 +1,27 @@
+
+package com.company;
+
+
+/**
+ *
+ * @author @version
+ */
+
+
+public class MyGame {
+
+    /**
+    *
+    * @param nothing
+    */
+    public static void main(String[] args) {
+        Game game = new Game();
+        game.play(); 
+        
+    }
+    
+    
+    
+}
+
+
